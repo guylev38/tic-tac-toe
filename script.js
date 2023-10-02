8d8c716770f3f9e0af7c6bfc906c58352ed2ab80
Guy <guydovlev@gmail.com>
2023-10-02 22:18:00 +0300

    Reseting the pc

--- script.js
+++ script.js
@@ -1,23 +1,50 @@
-const Gameboard = function (data) {
-  this.data = data;
+let currentPlayer = 'x';
+
+const Gameboard = function () {
+  this.data = [
+    ['x', 'o', 'x'],
+    ['x', 'o', 'o'],
+    ['x', 'x', 'o'],
+  ];
+  this.gameboardDiv = document.querySelector('.gameboard');
   this.drawBoard = () => {
-    const gameboardDiv = document.querySelector('.gameboard');
-    for (let i = 0; i < 9; i++) {
-      let card = document.createElement('div');
-      card.classList.add('card');
-      if (this.data[i] == 'x') {
-        card.textContent = 'X';
-        card.id = 'x';
-      } else if (this.data[i] == 'o') {
-        card.textContent = 'O';
-        card.id = 'o';
+    for (let i = 0; i < 3; i++) {
+      for (let j = 0; j < 3; j++) {
+        let card = document.createElement('div');
+        card.classList.add('card');
+        card.addEventListener('click', () => {
+          card.textContent = currentPlayer.sign;
+          card.id = currentPlayer.sign;
+        });
+        if (this.data[i][j] == 'x') {
+          card.textContent = 'X';
+          card.id = 'x';
+        } else if (this.data[i][j] == 'o') {
+          card.textContent = 'O';
+          card.id = 'o';
+        }
+        this.gameboardDiv.appendChild(card);
       }
-      gameboardDiv.appendChild(card);
     }
   };
+  this.checkWin = () => {
+    // Check Rows;
+    for (let i = 0; i < 3; i++) {
+      for (let j = 0; j < 3; j++) {}
+    }
+  };
+  this.highlightWin;
 };
 
-let data = ['', '', '', '', '', '', '', '', ''];
+const Player = function (sign) {
+  this.sign = sign;
+};
 
-const gameboard = new Gameboard(data);
+const gameboard = new Gameboard();
 gameboard.drawBoard();
+
+const playerX = new Player('X');
+const playerO = new Player('O');
+
+const players = [playerX, playerO];
+currentPlayer = players[0];
