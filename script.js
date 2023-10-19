d20afc5358d23599686671cfa20ec6391445424d
guy <guydovlev@gmail.com>
2023-10-20 00:17:42 +0300

    Need to fix game loop

--- script.js
+++ script.js
@@ -1,27 +1,40 @@
-let currentPlayer = 'x';
+const Player = function (sign) {
+  this.sign = sign;
+};
+
+const playerX = new Player("X");
+const playerO = new Player("O");
+
+const players = [playerX, playerO];
+
+let currentPlayer = players[0];
 
 const Gameboard = function () {
   this.data = [
-    ['x', 'o', 'o'],
-    ['o', 'x', 'x'],
-    ['o', 'x', 'x'],
+    ["", "", ""],
+    ["", "", ""],
+    ["", "", ""],
   ];
-  this.gameboardDiv = document.querySelector('.gameboard');
+  this.prompt = document.getElementById("prompt");
+  this.gameboardDiv = document.querySelector(".gameboard");
   this.drawBoard = () => {
     for (let i = 0; i < 3; i++) {
       for (let j = 0; j < 3; j++) {
-        let card = document.createElement('div');
-        card.classList.add('card');
-        card.addEventListener('click', () => {
+        isTurnPlayed = false;
+        let card = document.createElement("div");
+        card.classList.add("card");
+        card.addEventListener("click", () => {
           card.textContent = currentPlayer.sign;
           card.id = currentPlayer.sign;
+          isTurnPlayed = true;
+          switchTurn();
         });
-        if (this.data[i][j] == 'x') {
-          card.textContent = 'X';
-          card.id = 'x';
-        } else if (this.data[i][j] == 'o') {
-          card.textContent = 'O';
-          card.id = 'o';
+        if (this.data[i][j] == "x") {
+          card.textContent = "X";
+          card.id = "x";
+        } else if (this.data[i][j] == "o") {
+          card.textContent = "O";
+          card.id = "o";
         }
         this.gameboardDiv.appendChild(card);
       }
@@ -30,44 +43,46 @@ const Gameboard = function () {
   this.checkWin = () => {
     // Check Rows;
     for (let i = 0; i < 3; i++) {
-      middle = this.data[i][1]
-      left = this.data[i][0]
-      right = this.data[i][2]
-      if(middle == left && middle == right) return true;
+      middle = this.data[i][1];
+      left = this.data[i][0];
+      right = this.data[i][2];
+      if (middle == left && middle == right) return true;
     }
 
     // Check Cols
-    for(let j=0; j<3; j++){
+    for (let j = 0; j < 3; j++) {
       middle = this.data[1][j];
       upper = this.data[0][j];
       bottom = this.data[2][j];
-      if(middle == upper && middle == bottom) return true;
+      if (middle == upper && middle == bottom) return true;
     }
 
     // Check Main Diagonal
-    if(this.data[0][0] == this.data[1][1] && this.data[0][0] == this.data[2][2])
-      return true;    
-
-
-    // Check Sub Diagonal
-    if(this.data[0][2] == this.data[1][1] && this.data[1][1] == this.data[2][0])
+    if (
+      this.data[0][0] == this.data[1][1] &&
+      this.data[0][0] == this.data[2][2]
+    )
       return true;
 
+    // Check Sub Diagonal
+    if (
+      this.data[0][2] == this.data[1][1] &&
+      this.data[1][1] == this.data[2][0]
+    )
+      return true;
 
+    this.prompt.textContent = `${currentPlayer.sign.toUpperCase()} Won The Game!`;
     return false;
   };
-  // this.highlightWin;
 };
 
-const Player = function (sign) {
-  this.sign = sign;
-};
+let gameboard = new Gameboard();
+const startButton = document.querySelector(".start-button");
 
-const gameboard = new Gameboard();
-gameboard.drawBoard();
+startButton.addEventListener("click", gameboard.drawBoard());
 
-const playerX = new Player('X');
-const playerO = new Player('O');
-
-const players = [playerX, playerO];
-currentPlayer = players[0];
+function switchTurn() {
+  if (gameboard.checkWin()) return;
+  if (currentPlayer == players[0]) currentPlayer = players[1];
+  else if (currentPlayer == players[1]) currentPlayer = players[0];
+}
